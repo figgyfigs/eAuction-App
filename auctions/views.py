af94521704e176a885d094d7037d3e1346d4b499
figgyfigs <a.figs@protonmail.com>
2020-10-13 21:51:21 -0700

    Initial commit

---
+++ auctions/views.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from .models import User
+
+
+def index(request):
+    return render(request, "auctions/index.html")
+
+
