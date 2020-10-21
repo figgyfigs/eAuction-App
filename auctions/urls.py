11eae428754c2a72cfa0c22befa154913025d011
figgyfigs <a.figs@protonmail.com>
2020-10-21 09:34:38 -0700

    Users can now register for an account. Changes to urls

--- auctions/urls.py
+++ auctions/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login")
-
+    path("login", views.login_view, name="login"),
+    path("register", views.register, name="register")
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -29,3 +29,29 @@ def login_view(request):
     else:
         return render(request, "auctions/login.html")
 
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "auctions/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "auctions/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/register.html")
